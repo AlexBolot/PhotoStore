0b573fc17c24bce39f168acc5bbfe771b6cd9c3e
AlexBolot <bolotalex06@gmail.com>
2021-01-03 23:39:53 +0100

    - Adding copyrights - Adding shared config.dart (with all constants and global access) - Updating main.dart with the Splashscreen and removing default generated code - Removing "test/" folder (unused)

---
+++ lib/config.dart
@@ -0,0 +1,47 @@
+/*..............................................................................
+ . Copyright (c)
+ .
+ . The config.dart class was created by : Alex Bolot and Pierre Bolot
+ .
+ . As part of the PhotoStore project
+ .
+ . Last modified : 1/3/21 11:38 PM
+ .
+ . Contact : contact.alexandre.bolot@gmail.com
+ .............................................................................*/
+
+import 'package:flutter/material.dart';
+
+//========== Page Routes ==========//
+
+// TODO - add page routes here
+// Example : String get accountView => '/AccountView';
+
+//========== FormFields Validators ==========//
+
+RegExp emailRegExp = RegExp(
+    r"^((([A-Za-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([A-Za-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([A-Za-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([A-Za-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([A-Za-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([A-Za-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([A-Za-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([A-Za-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([A-Za-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([A-Za-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$");
+
+FormFieldValidator<String> notEmpty = (value) => (value ?? '').isEmpty ? 'Vous devez entrer une valeur' : null;
+
+FormFieldValidator<String> isEmail = (value) {
+  String provided = notEmpty(value);
+  String isEmail;
+
+  if (!emailRegExp.hasMatch(value)) isEmail = "Cet adresse n'est pas valide";
+
+  return provided ?? isEmail;
+};
+
+FormFieldValidator<String> validPassword = (value) {
+  String provided = notEmpty(value);
+  String validPassword;
+
+  if (value.length <= 6) validPassword = 'Minimum 6 charactères';
+
+  return provided ?? validPassword;
+};
+
+//========== Others ==========//
+
+getTime() => DateTime.now().millisecondsSinceEpoch;
